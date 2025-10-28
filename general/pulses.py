e55e4699ffbd92318e7a046d9872a0ad1cdb8f95
Hafnium <thywissenlab.rpi02@gmail.com>
2025-10-28 15:43:17 -0400

    Created pulses.py and example

---
+++ general/pulses.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.constants import pi
+
+def blackman_pulse(t, t_width, alpha=0.16):
+    a0, a1, a2 = (1 - alpha)/2, 0.5, alpha/2
+    def blackman(t):
+        return a0 - a1*np.cos(2*pi*t/t_width) + a2*np.cos(4*pi*t/t_width)
+    conditions = [t < 0, (t >= 0) & (t <= t_width), t > t_width]
+    functions = [0, blackman, 0]  
+    return np.piecewise(t, conditions, functions)
+
+
+def square_pulse(t, t_width):
+    conditions = [t < 0, (t >= 0) & (t <= t_width), t > t_width]
+    functions = [0, 1, 0]  
+    return np.piecewise(t, conditions, functions)
