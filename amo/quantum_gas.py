463b04645417781c327196c87818e83cf52d1eb8
colindebin <colindeb1n2@gmail.com>
2026-01-08 13:22:14 -0500

    Some fixes to Fermi wavevector

--- amo/quantum_gas.py
+++ amo/quantum_gas.py
@@ -8,23 +8,23 @@ def thermal_deBroglie_wavelength(T):
     return h / np.sqrt(2 * np.pi * mK * kB * T)
 
 
-def fermi_energy(n, omega_bar, dim=3, pol=1):
-    """Computes the Fermi energy in Joules for a given number of atoms n
+def fermi_energy(N, omega_bar, dim=3, pol=1):
+    """Computes the Fermi energy in Joules for a given number of atoms N
     and geometric mean trap frequency omega_bar (in rad/s)."""
     if dim == 3:
-        return hbar * omega_bar * (6 * n * pol)**(1/3)
+        return hbar * omega_bar * (6 * N * pol)**(1/3)
     elif dim == 2:
-        return hbar * omega_bar * np.sqrt(2 * n * pol)
+        return hbar * omega_bar * np.sqrt(2 * N * pol)
     elif dim == 1:
-        return hbar * omega_bar * n * pol
+        return hbar * omega_bar * N * pol
     else:
         raise ValueError("Dimension dim must be 1, 2, or 3.")
 
 
-def fermi_wavevector(n, omega_bar, dim=3, pol=1):
-    """Computes the Fermi wavevector in 1/m for a given number density 
-    n (in 1/m^3) and geometric mean trap frequency omega_bar (in rad/s)."""
-    return np.sqrt(2 * mK * fermi_energy(n, omega_bar, dim=dim, pol=pol))
+def fermi_wavevector(N, omega_bar, dim=3, pol=1):
+    """Computes the Fermi wavevector in 1/m for a given atom number  
+    N and geometric mean trap frequency omega_bar (in rad/s)."""
+    return np.sqrt(2 * mK * fermi_energy(N, omega_bar, dim=dim, pol=pol))/hbar
 
 
 def oscillator_length(omega):
